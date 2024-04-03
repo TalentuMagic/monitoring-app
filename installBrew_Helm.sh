9f635312aa11328f67e232d316fce9691f9e62e3
TalentuMagic <davidioan2001@gmail.com>
2024-04-03 21:13:13 +0300

    updated script

--- installBrew_Helm.sh
+++ installBrew_Helm.sh
@@ -2,7 +2,7 @@
 # Homebrew
 echo "Installing Homebrew...";
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" | echo
-(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/ubuntu/.bashrc
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> $HOME/.bashrc
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew install gcc
 echo "Done";
