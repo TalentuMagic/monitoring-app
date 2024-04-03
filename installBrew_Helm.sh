09a7041bac41e36543cd329ab60f17906cb082e3
TalentuMagic <davidioan2001@gmail.com>
2024-04-03 19:58:52 +0300

    auto-insall Brew

--- installBrew_Helm.sh
+++ installBrew_Helm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Homebrew
 echo "Installing Homebrew...";
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" | echo
 (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/ubuntu/.bashrc
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew install gcc -y
