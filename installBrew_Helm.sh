a1bbd18a6524decf412e72d7a473589f9789679a
TalentuMagic <davidioan2001@gmail.com>
2024-04-03 22:19:39 +0300

    fix

--- installBrew_Helm.sh
+++ installBrew_Helm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Homebrew
-home_dir = $(eval echo ~$USER)
+home_dir=$(eval echo ~$USER);
 echo "Installing Homebrew...";
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" | echo
 (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> "$home_dir"/.bashrc
